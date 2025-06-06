92c162d79b38b90851e397465291e81143685e3e
杨晨 <165390625+MyrsYC@users.noreply.github.com>
2025-06-06 11:45:30 +0800

    基于feign的远程调用

--- cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,20 +1,37 @@
 package cn.itcast.order.service;
 
+
+import cn.itcast.feign.clients.UserClient;
+import cn.itcast.feign.pojo.User;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
-import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @Service
 public class OrderService {
 
     @Autowired
     private OrderMapper orderMapper;
-    @Autowired
-    private RestTemplate restTemplate;
 
+    @Resource
+    private UserClient userClient;
+
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+        // 2.利用Feign发起Http请求，查询用户
+        User user=userClient.findById(order.getUserId());
+        // 3. 封装user到Order
+        order.setUser(user);
+        // 4.返回
+        return order;
+    }
+
+    /*@Autowired
+    private RestTemplate restTemplate;
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
@@ -25,5 +42,5 @@ public class OrderService {
         order.setUser(user);
         // 4.返回
         return order;
-    }
+    }*/
 }
