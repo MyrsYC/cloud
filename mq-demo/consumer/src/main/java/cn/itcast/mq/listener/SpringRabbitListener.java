4aa90eb0b9fb85aada6c24548a16f0923f583d42
杨晨 <165390625+MyrsYC@users.noreply.github.com>
2025-06-08 15:46:04 +0800

    工作队列的学习使用

--- mq-demo/consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
+++ mq-demo/consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
@@ -1,12 +1,28 @@
 package cn.itcast.mq.listener;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
 
 @Component
 public class SpringRabbitListener {
+
+//    @RabbitListener(queues = "simple.queue")
+//    public void listenerSimpleQueue(String msg){
+//        System.out.println("消费者接收到simple.queue的消息:【"+msg+"】");
+//    }
+
     @RabbitListener(queues = "simple.queue")
-    public void listenerSimpleQueue(String msg){
-        System.out.println("消费者接收到simple.queue的消息:【"+msg+"】");
+    public void listenerWorkQueue1(String msg) throws InterruptedException {
+        System.out.println("消费者1接收到消息:【"+msg+"】"+ LocalTime.now());
+        Thread.sleep(20);
+    }
+
+    @RabbitListener(queues = "simple.queue")
+    public void listenerWorkQueue2(String msg) throws InterruptedException {
+        System.err.println("消费者2......接收到消息:【"+msg+"】"+ LocalTime.now());
+        Thread.sleep(200);
     }
 }
--- mq-demo/publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
+++ mq-demo/publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
@@ -20,4 +20,15 @@ public class SpringAmqpTest {
         rabbitTemplate.convertAndSend(queueName,message);
     }
 
+    @Test
+    public void testSendMessage2WorkQueue() throws InterruptedException {
+        String queueName="simple.queue";
+        String message="hello,message__";
+        for (int i = 0; i <50 ; i++) {
+            rabbitTemplate.convertAndSend(queueName,message+i);
+            Thread.sleep(20);
+        }
+
+    }
+
 }
