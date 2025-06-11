a842c2ca161e3aff68d0393c8a3ec6b677f4da5b
杨晨 <165390625+MyrsYC@users.noreply.github.com>
2025-06-11 10:24:00 +0800

    黑马旅游案例-广告置顶（算分函数查询）

--- hotel-demo/src/main/java/cn/itcast/hotel/service/impl/HotelService.java
+++ hotel-demo/src/main/java/cn/itcast/hotel/service/impl/HotelService.java
@@ -16,6 +16,8 @@ import org.elasticsearch.common.geo.GeoPoint;
 import org.elasticsearch.common.unit.DistanceUnit;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.functionscore.FunctionScoreQueryBuilder;
+import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.sort.SortBuilders;
@@ -98,7 +100,23 @@ public class HotelService extends ServiceImpl<HotelMapper, Hotel> implements IHo
         if (minPrice!=null&&maxPrice!=null) {
             boolQuery.filter(QueryBuilders.rangeQuery("price").lte(maxPrice).gte(minPrice));
         }
-        request.source().query(boolQuery);
+
+        //算分控制
+        FunctionScoreQueryBuilder functionScoreQuery =
+                QueryBuilders.functionScoreQuery(
+                        //原始查询，相关性算分查询
+                        boolQuery,
+                        //function score的数组
+                        new FunctionScoreQueryBuilder.FilterFunctionBuilder[]{
+                                //其中一个的function score元素
+                                new FunctionScoreQueryBuilder.FilterFunctionBuilder(
+                                        //过滤条件
+                                        QueryBuilders.termQuery("isAD",true),
+                                        //算分函数
+                                        ScoreFunctionBuilders.weightFactorFunction(10)
+                                )
+                        });
+        request.source().query(functionScoreQuery);
     }
 
     private PageResult handleResponse(SearchResponse response) {
