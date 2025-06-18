3818429645092addc7fd2f3c3dc43dc32583716c
杨晨 <165390625+MyrsYC@users.noreply.github.com>
2025-06-18 22:25:26 +0800

    消息持久化

--- mq-advanced-demo/publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
+++ mq-advanced-demo/publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
@@ -3,11 +3,16 @@ package cn.itcast.mq.spring;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageDeliveryMode;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 @Slf4j
@@ -43,5 +48,15 @@ public class SpringAmqpTest {
         });
         //3. 发送消息
         rabbitTemplate.convertAndSend("amq.topic", "simple.test", message, correlationData);
+    }
+
+    @Test
+    public void testDurableMessage(){
+        //1. 准备消息
+        Message message = MessageBuilder.withBody("hello, Spring ".getBytes(StandardCharsets.UTF_8))
+                .setDeliveryMode(MessageDeliveryMode.PERSISTENT)
+                .build();
+        //2. 发送消息
+        rabbitTemplate.convertAndSend("simple.queue",message);
     }
 }
